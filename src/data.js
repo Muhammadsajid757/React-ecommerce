ba24f723ecfd016872993069b433b9aeceaba87f
Muhammad Sajid <sajid03157@gmail.com>
2023-06-25 20:04:39 -0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://tse1.mm.bing.net/th?id=OIP.7ECOnUawom6JQO09uo8UogHaJ4&pid=Api&rs=1&c=1&qlt=95&w=89&h=119",
+    title: "SUMMER SALE",
+    desc: "WE are working for quality not for quantity.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://tse1.mm.bing.net/th?id=OIP.077N83KIUGLUO-cVWeBYDAHaJS&pid=Api&rs=1&c=1&qlt=95&w=95&h=119",
+    title: "AUTUMN SALE",
+    desc: "it's big oppurtunity for you to buy online",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/cXFnLLV/3.png",
+    title: "LOUNGEWEAR SALE",
+    desc: "You can get discount  30% OFF on Eid.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://rlv.zcache.com/1st_fathers_day_photo_collage_baby_t_shirt-r_d9fwh_200.jpg",
+    title: "T SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: "https://m.media-amazon.com/images/I/71Rr7NQQZNL._AC_UL400_.jpg",
+    title: "LOUNGEWEAR STYLE",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    title: "LIGHT JACKETS STYLE",
+  },
+];
+
+export const popularProducts = [
+  {
+    id:1,
+    img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+  },
+  {
+    id:2,
+    img:"https://tse1.mm.bing.net/th?id=OIP.1gyuITxSroERpV2o_yjluAHaKO&pid=Api&rs=1&c=1&qlt=95&w=87&h=120",
+  },
+  {
+    id:3,
+    img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+  },
+  {
+    id:4,
+    img:"https://tse2.mm.bing.net/th?id=OIP.oca-l2Hruaijlfms052DIgHaKg&pid=Api&P=0&h=180",
+  },
+  {
+    id:5,
+    img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id:6,
+    img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id:7,
+    img:"https://tse1.mm.bing.net/th?id=OIP.1HxqlQfY4E4WWhhemX7b6gHaGs&pid=Api&P=0&h=180",
+  },
+  {
+    id:8,
+    img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+]
